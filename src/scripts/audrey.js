61180f74889d1dade397ec1180d0fa2f7bf57cd7
James Chapman <chapmanjamesg@gmail.com>
2019-10-16 14:45:17 -0500

    this sucks, I barely understand the instructions...

---
+++ src/scripts/audrey.js
@@ -0,0 +1,28 @@
+console.log('They come to snuff the rooster....')
+
+
+const audrey = document.getElementById("audrey")
+
+/*
+    Add an event listener to the `document` object to listen
+    for the "scroll" event.
+*/
+.addEventListener("scroll", function (event) {
+    /*
+        Adjust the width of audrey to be 1/3 the value of
+        `window.scrollY`. No lower than 50px, though.
+    */
+
+    if(audrey.style.width/3 > 50) {
+        window.srollY
+    }else if(audrey.style.width/4 >100) {
+
+    }else {
+
+    }
+
+    /*
+        Adjust the height of audrey to be 1/4 the value of
+        `window.scrollY`. No lower than 100px, though.
+    */
+})
